49eed8876846b503f0cce719c8197ed5649e7073
Marcelo Colletti <mcolletti@Marcelo-Ubuntu.(none)>
2014-11-03 15:14:51 -0200

    Adicionado pi.c que calcula casas do PI

---
+++ src/pi.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SCALE 10000
+#define ARRINIT 2000
+
+void pi_digits(int );
+
+int main (){
+
+   clock_t start, end;
+   double time;
+   
+   start = clock();
+   pi_digits(200000);
+   end = clock();
+
+   time = ((double) (end - start)) / CLOCKS_PER_SEC;
+   printf("time = %lf\n", time);
+
+   return 0;
+
+}
+
+void pi_digits(int digits) {
+    int carry = 0;
+    int arr[digits + 1];
+    for (int i = 0; i <= digits; ++i)
+        arr[i] = ARRINIT;
+    for (int i = digits; i > 0; i-= 14) {
+        int sum = 0;
+        for (int j = i; j > 0; --j) {
+            sum = sum * j + SCALE * arr[j];
+            arr[j] = sum % (j * 2 - 1);
+            sum /= j * 2 - 1;
+        }
+        printf("%04d", carry + sum / SCALE);
+        carry = sum % SCALE;
+    }
+   printf("\n");
+}
